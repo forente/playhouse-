26acc4d19e6786b0269f1165dd96ac6c79c7b156
forente <zerofox16@gmail.com>
2016-09-30 00:11:21 -0500

    ticking clock

---
+++ New Unity Project/Assets/ClockAnimator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	public Transform hours, minutes, seconds;
+
+	private const float
+		hoursToDegrees = 360f / 12f,
+		minutesToDegrees = 360f / 60f,
+		secondsToDegrees = 360f / 60f;
+
+	private void Update(){
+		DateTime time = DateTime.Now;
+
+		hours.localRotation =
+			Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
+		minutes.localRotation =
+			Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees);
+		seconds.localRotation =
+			Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+	}
+
+}
