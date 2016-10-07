33ac2c24c1a051d4a6d796e717c7e8034f9c30ab
forente <zerofox16@gmail.com>
2016-10-06 23:19:22 -0500

    fract

--- New Unity Project/Assets/Fractal.cs
+++ New Unity Project/Assets/Fractal.cs
@@ -5,10 +5,21 @@ public class Fractal : MonoBehaviour {
 
 	public Mesh mesh;
 	public Material material;
-	
+
+
+	public int maxDepth;
+	private int depth;
 
 	private void Start () {
 		gameObject.AddComponent<MeshFilter> ().mesh = mesh;
 		gameObject.AddComponent<MeshRenderer> ().material = material;
+		if (depth < maxDepth){
+			new GameObject ("Fractal Child").AddComponent<Fractal> ();
+		}
+
+	}
+
+	private void Initialize (Fractal parent){
+	
 	}
 }
