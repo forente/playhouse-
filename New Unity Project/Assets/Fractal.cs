b21ea84b7f54be206874fee7ebbd83be31061c7b
forente <zerofox16@gmail.com>
2016-10-08 14:40:42 -0500

    branches

--- New Unity Project/Assets/Fractal.cs
+++ New Unity Project/Assets/Fractal.cs
@@ -5,30 +5,39 @@ public class Fractal : MonoBehaviour {
 
 	public Mesh mesh;
 	public Material material;
-
-
 	public int maxDepth;
+	public float childScale;
+
 	private int depth;
 
 	private void Start () {
-		gameObject.AddComponent<MeshFilter> ().mesh = mesh;
-		gameObject.AddComponent<MeshRenderer> ().material = material;
-		if (depth < maxDepth){
-			new GameObject ("Fractal Child").AddComponent<Fractal> ().Initialize(this);
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;
+		if (depth < maxDepth) {
+			StartCoroutine(CreateChildren());
 		}
-
 	}
 
-	public float childScale;
+	private IEnumerator CreateChildren () {
+		yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.up, Quaternion.identity);
+		yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.right, Quaternion.Euler(0f, 0f, -90f));
+		yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+	}
 
-	private void Initialize (Fractal parent){
+	private void Initialize (Fractal parent,
+	                         Vector3 direction,
+	                         Quaternion orientation) {
 		mesh = parent.mesh;
 		material = parent.material;
 		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
 		childScale = parent.childScale;
-		depth = parent.depth++;
 		transform.parent = parent.transform;
 		transform.localScale = Vector3.one * childScale;
-		transform.localPosition = Vector3.up * (0.5f + 0.5f * childScale );
+		transform.localPosition = direction * (0.5f + 0.5f * childScale);
+		transform.localRotation = orientation;
 	}
 }
