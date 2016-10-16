072fa3f784ce8cfed8ac14ad67274e9adb0fca78
forente <zerofox16@gmail.com>
2016-10-15 20:50:38 -0500

    color vari

--- New Unity Project/Assets/Fractal.cs
+++ New Unity Project/Assets/Fractal.cs
@@ -3,23 +3,6 @@ using System.Collections;
 
 public class Fractal : MonoBehaviour {
 
-	public Mesh mesh;
-	public Material material;
-	public int maxDepth;
-	public float childScale;
-
-	private int depth;
-
-	private void Start () {
-		gameObject.AddComponent<MeshFilter>().mesh = mesh;
-		gameObject.AddComponent<MeshRenderer>().material = material;
-		GetComponent<MeshRenderer>().material.color =
-			Color.Lerp(Color.white, Color.yellow, (float)depth / maxDepth);
-		if (depth < maxDepth) {
-			StartCoroutine(CreateChildren());
-		}
-	}
-
 	private static Vector3[] childDirections = {
 		Vector3.up,
 		Vector3.right,
@@ -36,18 +19,51 @@ public class Fractal : MonoBehaviour {
 		Quaternion.Euler(-90f, 0f, 0f)
 	};
 
+	public Mesh mesh;
+	public Material material;
+	public int maxDepth;
+	public float childScale;
+
+	private int depth;
+	private Material[,] materials;
+
+	private void Start () {
+		if (materials == null) {
+			InitializeMaterials();
+		}
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material =
+			materials[depth, Random.Range(0, 2)];
+		if (depth < maxDepth) {
+			StartCoroutine(CreateChildren());
+		}
+	}
+	
+	private void InitializeMaterials () {
+		materials = new Material[maxDepth + 1, 2];
+		for (int i = 0; i <= maxDepth; i++) {
+			float t = i / (maxDepth - 1f);
+			t *= t;
+			materials[i, 0] = new Material(material);
+			materials[i, 0].color = Color.Lerp(Color.white, Color.yellow, t);
+			materials[i, 1] = new Material(material);
+			materials[i, 1].color = Color.Lerp(Color.white, Color.cyan, t);
+		}
+		materials[maxDepth, 0].color = Color.magenta;
+		materials[maxDepth, 1].color = Color.red;
+	}
+	
 	private IEnumerator CreateChildren () {
 		for (int i = 0; i < childDirections.Length; i++) {
 			yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
 			new GameObject("Fractal Child").AddComponent<Fractal>().
-			Initialize(this, i);
+				Initialize(this, i);
 		}
 	}
 
-	private void Initialize (Fractal parent,
-	                         int childIndex) {
+	private void Initialize (Fractal parent, int childIndex) {
 		mesh = parent.mesh;
-		material = parent.material;
+		materials = parent.materials;
 		maxDepth = parent.maxDepth;
 		depth = parent.depth + 1;
 		childScale = parent.childScale;
