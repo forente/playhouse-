037a3977a67a61a9b41efc38d9ddd0133a27ef17
forente <zerofox16@gmail.com>
2016-10-27 19:46:04 -0500

    p

--- New Unity Project/Assets/movement.cs
+++ New Unity Project/Assets/movement.cs
@@ -3,13 +3,10 @@ using System.Collections;
 
 public class movement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
