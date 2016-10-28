4d7753cf854dc33c2c0700e5b2503d75ba39bf02
forente <zerofox16@gmail.com>
2016-10-28 00:19:37 -0500

    movement

--- New Unity Project/Assets/movement.cs
+++ New Unity Project/Assets/movement.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class movement : MonoBehaviour {
 
+	public float speed; 
+	private Rigidbody rb;
 
+	void Start (){
+		rb = GetComponent<Rigidbody>();
+		Debug.Log (rb);
+	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate () {
+		 
+		float moveHorizontal = Input.GetAxis ("Horizontal2");
+		float moveVertical = Input.GetAxis ("Vertical2");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
 	}
 }
