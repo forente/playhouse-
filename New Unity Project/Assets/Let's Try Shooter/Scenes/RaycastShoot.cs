585417dfb36dc262c0fa77fd0c60515c22681f50
forente <zerofox16@gmail.com>
2016-12-14 18:52:24 -0600

    raycast Script

---
+++ New Unity Project/Assets/Let's Try Shooter/Scenes/RaycastShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour {
+
+	public int gunDamage = 1; 
+	public float fireRate = 0.25f;
+	public float weaponRange = 50f; 
+	public float hitForce = 100f;
+	public Transform gunEnd;
+
+	private Camera fpsCam;
+	private WaitForSeconds shotDuration = new WaitForSeconds(0.7f);
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
