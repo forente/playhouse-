206653f504c6abc8ce3b55c21ccf6a2fd43c6792
forente <zerofox16@gmail.com>
2017-01-15 23:36:38 -0600

    platformer

---
+++ simpPlatform/Assets/scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState{
+	menu,
+	inGame,
+	gameOver
+}
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+	public GameState currentGameState = GameState.menu;
+
+	void Awake(){
+		instance = this;
+	}
+
+
+	// called when the game starts 
+	public void startGame(){
+	
+	}
+
+	// called when failure condition is met
+	public void gameOver(){
+	
+	}
+
+	// called to take player back to main menu
+	public void backToMenu(){
+	
+	}
+
+	void setGameState(GameState newGameState){
+		if (newGameState == GameState.menu) {
+
+		}
+
+		else if(newGameState == GameState.inGame){
+			
+		}
+
+		else if(newGameState == GameState.gameOver){
+			
+		}
+
+		currentGameState = newGameState;
+	}
+}
---
+++ simpPlatform/Assets/scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float jumpForce = 25f;
+	public float runningSpeed = 1.5f;
+	public LayerMask groundLayer; 
+	private Rigidbody2D rigidbody;
+	public Animator animator; 
+
+
+	void Awake(){
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			jump ();
+		}
+		animator.SetBool ("isGrounded", isGrounded());
+	}
+
+	void FixedUpdate(){
+		if(rigidbody.velocity.x < runningSpeed){
+			rigidbody.velocity = new Vector2 (runningSpeed , rigidbody.velocity.y);
+		}
+	}
+
+	void Start(){
+		animator.SetBool ("isAlive", true);
+	}
+
+	void jump(){
+		if(isGrounded()){
+			rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+		}
+	}
+
+	bool isGrounded(){
+		if(Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundLayer)){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
