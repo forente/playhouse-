53830a6898fd9c77ecb359477ca0974997455f03
forente <zerofox16@gmail.com>
2017-03-03 13:28:30 -0600

    Num wizard

---
+++ Number wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		print("");
+		print("");
+
+		print("");
+		print("");
+
+		print("");
+		print("");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
