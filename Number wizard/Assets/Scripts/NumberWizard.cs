749e7f11f4ad28b9c1ecf47f4e1cec4f80e6c28c
forente <zerofox16@gmail.com>
2017-03-05 23:04:25 -0600

    Added random guesses.

--- Number wizard/Assets/Scripts/NumberWizard.cs
+++ Number wizard/Assets/Scripts/NumberWizard.cs
@@ -10,14 +10,36 @@ public class NumberWizard : MonoBehaviour {
 	int guess;
 
 	void Start () {
+		startGame();
+	}
 	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			
+			//print("Up arrow pressed");	
+			min = guess;
+			nextGuess();
+		}else if (Input.GetKeyDown(KeyCode.DownArrow)){
+			
+			//print("Down arrow pressed");
+			max = guess;
+			nextGuess();
+		}else if (Input.GetKeyDown(KeyCode.Return)){
+			print("I have won!");	
+			startGame();
+		}
+	}
+
+	void startGame(){
 		 max = 1000;
 		 min = 1;
 		 guess = 500; 
+		 print("**************************");
 		print("Welcome to Number wizard! ");
 		print("Pick an number in your head and hold onto it.");
 
-		 max = 1000;
+		 max = 1001;
 		 min = 1;
 
 		print("The highest number you can pick is " +max);
@@ -26,21 +48,10 @@ public class NumberWizard : MonoBehaviour {
 		print("Is the number higher of lower that 500?");
 		print("Up = Higher, Down = Lower, return");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow)){
-			//print("Up arrow pressed");	
-			min = guess;
-			guess  =  (max + min)/2;
-			print("higher or lower that " +guess);
-		}else if (Input.GetKeyDown(KeyCode.DownArrow)){
-			//print("Down arrow pressed");
-			max = guess;
-			guess  =  (max + min)/2;
-			print("higher or lower that " +guess);	
-		}else if (Input.GetKeyDown(KeyCode.Return)){
-			print("I have won!");	
-		}
+
+	void nextGuess(){
+		guess  =  Random.Range(min, max);
+		print("higher or lower that " +guess);
+		print("Up = Higher, Down = Lower, return");
 	}
 }
