caf16630eb4c32e1b0485271da4705b498e687b9
forente <zerofox16@gmail.com>
2017-03-08 16:46:25 -0600

    Adventure game added

---
+++ Text Advneture/Assets/TextController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	enum States
+	{
+		
+	}
+	// Use this for initialization
+	void Start () {
+		text.text = "Are you ready to continue on your adventure?";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			text.text = "You find yourslef on the coast of the sea town of Moleno. "+
+						"The inhabittance of the town seem to be concerned about your sudden appearance. \n\n" +
+						" What do you do?";
+		}
+	}
+}
